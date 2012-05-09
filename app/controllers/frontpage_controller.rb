1a47605b3e4f74288c6cdad008b001e3b6e0b508
Joseph Method <tristil@gmail.com>
2012-05-09 00:13:24 -0400

    Add frontpage

---
+++ app/controllers/frontpage_controller.rb
@@ -0,0 +1,4 @@
+class FrontpageController < ApplicationController
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ MethodGtd::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'frontpage#index'
 
   # See how all your routes lay out with "rake routes"
 
