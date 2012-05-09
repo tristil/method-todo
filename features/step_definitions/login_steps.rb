7e3ab26ae357e52333326d1c11a0f84c55f99cd0
Joseph Method <tristil@gmail.com>
2012-05-09 02:42:36 -0400

    Signup feature committed

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -16,5 +16,17 @@ end
 
 Then /I should be taken to the signup page/ do
   page.status_code.should == 200
+  page.should have_content("Password confirmation")
+end
+
+And /when I enter an email and password and submit form/ do
+  fill_in('user[email]', :with => 'newuser@example.com')
+  fill_in('user[password]', :with => 'Password1')
+  fill_in('user[password_confirmation]', :with => 'Password1')
+  click_button("Sign up")
+end
+
+Then /I should be logged in/ do
+  page.should have_content("Welcome!")
 end
 
