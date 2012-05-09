3c54bd73f2c7fd1be304682d91929dc52b4a29ce
Joseph Method <tristil@gmail.com>
2012-05-09 00:00:53 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MethodGtd::Application.config.secret_token = 'aaf6cb83e275853df3badc1904e6f3e503d54432fe45a7cf521d668004bea96735eccfa355ff0939b8a916a878d131913dec1ae42fedcd2c2d7f221db30952fe'
