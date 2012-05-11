e3ae350cee9c23d289ac61546d7053aa8bc008bf
Joseph Method <tristil@gmail.com>
2012-05-11 15:05:15 -0400

    Add specs

---
+++ spec/helpers/frontpage_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+# Specs in this file have access to a helper object that includes
+# the FrontpageHelper. For example:
+#
+# describe FrontpageHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       helper.concat_strings("this","that").should == "this that"
+#     end
+#   end
+# end
+describe FrontpageHelper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
