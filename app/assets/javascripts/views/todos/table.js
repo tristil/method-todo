26e5a134fb4b215e847efe7a2d67358094c27a9f
Joseph Method <tristil@gmail.com>
2012-06-19 17:29:34 -0400

    Fix deleting
    Conflicts:
    
    	app/assets/javascripts/views/todos/delete_modal.js
    	app/assets/javascripts/views/todos/table.js

--- app/assets/javascripts/views/todos/table.js
+++ app/assets/javascripts/views/todos/table.js
@@ -24,6 +24,15 @@ MethodTodo.Views.TodoTable = Backbone.View.extend({
     this.collection.bind('sync', this.updateTodoDescription, this);
   },
 
+  openDeleteModal : function(event)
+  {
+    event.preventDefault();
+    var delete_link = $(event.currentTarget);
+    var id = parseInt(delete_link.attr('id').replace("todo-delete-", ""));
+    $('#delete-todo-id').html(id);
+    $('#delete-todo-modal').modal('show');
+  },
+
   toggleCheckbox : function(event)
   {
     var self = this;
