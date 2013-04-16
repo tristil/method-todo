26962db30db1fb4c9218e895613dedae3c97f48c
Joseph Method <tristil@gmail.com>
2013-04-16 02:02:50 -0400

    Various attempts to get Capistrano working

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,17 +5,17 @@ set :scm, :git
 
 server = Capistrano::CLI.ui.ask "SSH server: "
 set(:user) { Capistrano::CLI.ui.ask "SSH username: " }
-set(:password) { Capistrano::CLI.ui.ask "SSH password: " }
 
 role :web, server                          # Your HTTP server, Apache/etc
 role :app, server                          # This may be the same as your `Web` server
 role :db,  server, :primary => true # This is where Rails migrations will run
 
 ssh_options[:forward_agent] = true
+ssh_options[:keys] = %w('~/.ssh/id_rsa.pub')
 default_run_options[:pty] = true
 
 set :use_sudo, false
-set :deploy_to, "~/production-sites/method-todo"
+set :deploy_to, "/home/#{user}/production-sites/method-todo"
 set :deploy_via, :remote_cache
 
 require 'rvm/capistrano'
