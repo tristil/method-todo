f9f8732e3d6fe65a36eee214aff7d041bb722df6
Joseph Method <tristil@gmail.com>
2013-10-05 00:28:07 -0400

    Roll back deploy changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,12 @@
+require 'bundler/capistrano'
+require 'capistrano-unicorn'
+# require 'sidekiq/capistrano'
+require 'dotenv/capistrano'
+
+set :default_environment, {
+  'PATH' => '/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH'
+}
+
 set :application, "method-todo"
 
 set :repository,  "git://github.com/tristil/method-todo.git"
@@ -13,32 +22,32 @@ end
 
 role :web, host # Your HTTP server, Apache/etc
 role :app, host # This may be the same as your `Web` server
-role :db, host, :primary => true # This is where Rails migrations will run
+role :db,  host, :primary => true # This is where Rails migrations will run
 
 ssh_options[:forward_agent] = true
 ssh_options[:keys] = %w('~/.ssh/id_rsa.pub')
 default_run_options[:pty] = true
 
-set :default_environment, {
-  'PATH' => '/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH'
-}
-
 set :use_sudo, false
 set :deploy_to, "/home/#{user}/production-sites/method-todo"
 set :deploy_via, :remote_cache
-
-require 'bundler/capistrano'
-require 'dotenv/capistrano'
+set :rails_env, :production
 
 # Do backup
 task :backup do
-  filename = "methodtodo.sql"
-  run "mysqldump -u root methodgtd > /tmp/#{filename}"
+  run "mysqldump -u root methodgtd > /tmp/methodtodo.sql"
 end
 
 task :set_credentials do
   upload("#{File.expand_path(File.dirname(__FILE__))}/../.env",
          "#{shared_path}/.env")
+end
+
+namespace :unicorn do
+  task :make_sockets_dir do
+    run "mkdir -p #{shared_path}/sockets"
+    run "ln -s #{shared_path}/sockets #{latest_release}/tmp/sockets"
+  end
 end
 
 before "deploy:migrate", "backup"
@@ -47,30 +56,8 @@ after "deploy:update", "deploy:cleanup"
 after "deploy:update", "deploy:migrate"
 before "deploy:finalize_update", "set_credentials"
 
-require 'capistrano-unicorn'
-
-after 'deploy:setup', 'unicorn:setup'
-after 'deploy:restart', 'unicorn:restart'
+before 'deploy:start', 'unicorn:make_sockets_dir'
 after 'deploy:start', 'unicorn:start'
+after 'deploy:restart', 'unicorn:restart'
+after 'deploy:reload', 'unicorn:reload'
 after 'deploy:stop', 'unicorn:stop'
-
-namespace :unicorn do
-  desc 'Release the unicorns!'
-  task :start, roles: :app, except: { no_release: true } do
-    run "sv start #{application}"
-  end
-
-  desc 'Nighty night, unicorns. Time for bed.'
-  task :stop, roles: :app, except: { no_release: true } do
-    run "sv stop #{application}"
-  end
-
-  desc 'Send a USR2 to the unicorn process for deploys.'
-  task :restart, roles: :app, except: { no_release: true } do
-    run "sv 2 #{application}"
-  end
-
-  task :setup, roles: :app do
-    run "mkdir -p #{shared_path}/sockets"
-  end
-end
---
+++ config/unicorn/production.rb
@@ -0,0 +1,33 @@
+app_path = File.expand_path(File.dirname(__FILE__) + "/../../")
+
+worker_processes 1
+preload_app true
+timeout 180
+
+working_directory app_path
+
+# Should be 'production' by default, otherwise use other env
+rails_env = ENV['RAILS_ENV'] || 'production'
+
+stderr_path "log/unicorn.log"
+stdout_path "log/unicorn.log"
+
+pid "#{app_path}/tmp/pids/unicorn.pid"
+listen "#{app_path}/tmp/sockets/unicorn.sock", :backlog => 64
+
+before_fork do |server, worker|
+  ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if File.exists?(old_pid) && server.pid != old_pid
+    begin
+      Process.kill("QUIT", File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+      # someone else did our job for us
+    end
+  end
+end
+
+after_fork do |server, worker|
+  ActiveRecord::Base.establish_connection
+end
